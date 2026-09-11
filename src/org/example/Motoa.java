b9f123491bc59455e3c616044149245891a51117
OyaunB <betxeberria24@izarraitz.eus>
2026-09-11 08:22:43 +0200

    aldaketa txiki batzuk (herentzia jarrita Motoak klasean)

--- src/org/example/Motoa.java
+++ src/org/example/Motoa.java
@@ -1,4 +1,8 @@
 package org.example;
 
-public class Motoa {
-}
+public class Motoa extends Ibilgailua{
+        public Motoa(String matrikula, String modeloa, String marka, int urtea) {
+            super(matrikula, modeloa, marka, urtea);
+        }
+    }
+
